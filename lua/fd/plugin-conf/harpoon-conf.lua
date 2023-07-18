5665c74d0c7994de6d37a619e0f87436a97ad364
Felix Ding <felixding1989@gmail.com>
2023-07-17 22:11:11 -0700

    Cleaner configuraiton for Harpoon

--- lua/fd/plugin-conf/harpoon-conf.lua
+++ lua/fd/plugin-conf/harpoon-conf.lua
@@ -1,15 +1,17 @@
 require("harpoon").setup({
   menu = {
-    width = vim.api.nvim_win_get_width(0) - 4,
+    width = vim.api.nvim_win_get_width(0) - 40,
+    tabline = false,
+    tabline_prefix = "  ",
+    tabline_suffix = "  "
   }
 })
 
-vim.keymap.set("n", "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>u", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", {noremap=true})
+vim.keymap.set("n", "<leader>mm", "<cmd>lua require('harpoon.mark').add_file()<CR>", {noremap=true})
+vim.keymap.set("n", "<leader>uu", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", {noremap=true})
 vim.keymap.set("n", "<leader>ne", "<cmd>lua require('harpoon.ui').nav_next()<CR>", {noremap=true})
 vim.keymap.set("n", "<leader>nw", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", {noremap=true})
 
-vim.keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", {noremap=true})
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, "<cmd>lua require('harpoon.ui').nav_file(" .. i .. ")<CR>", {noremap=true})
+end
