599c1320124fd5de31029e48f985ffdd14c8f7ea
Felix Ding <felixding1989@gmail.com>
2023-08-01 00:09:46 -0700

    Removes vim background color

--- lua/fd/init.lua
+++ lua/fd/init.lua
@@ -1,2 +1,5 @@
 require("fd.plugins")
 
+-- background color adjustment to see background picture
+vim.cmd[[highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight LineNr guibg=NONE ctermbg=NONE]]
