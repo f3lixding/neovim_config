5429277b8f3c8f949029e3bf135249a2ce138fb7
Felix Ding <felixding1989@gmail.com>
2023-11-11 21:07:29 -0800

    Adds fzf extension settings

--- after/plugin/telescope-conf.lua
+++ after/plugin/telescope-conf.lua
@@ -24,6 +24,14 @@ require('telescope').setup {
             }
         },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+        }
+    }
 }
 
 require("telescope").load_extension("live_grep_args")
