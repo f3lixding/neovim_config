ab6890870290598c7f2cdee146dd1ec83969c88c
Felix Ding <felixding1989@gmail.com>
2023-11-12 12:38:38 -0800

    Adds current buffer fuzzy find and adjusts some telescope hotkey binding

--- after/plugin/telescope-conf.lua
+++ after/plugin/telescope-conf.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+vim.keymap.set('n', '<leader>fk', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
     { desc = "Live Grep" })
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Find Symbols" })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fn', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fl', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", { desc = "Current Buffer Fuzzy Find" })
 
 local actions = require "telescope.actions"
 
