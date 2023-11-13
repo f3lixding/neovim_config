f94c8e12a490b1609f1c2450f710272a33d7b437
Felix Ding <felixding1989@gmail.com>
2023-11-12 16:38:38 -0800

    Makes border background color transparent

--- lua/fd/init.lua
+++ lua/fd/init.lua
@@ -3,3 +3,6 @@ require("fd.plugins")
 -- background color adjustment to see background picture
 vim.cmd[[highlight Normal guibg=NONE ctermbg=NONE]]
 vim.cmd[[highlight LineNr guibg=NONE ctermbg=NONE]]
+
+-- background color for borders
+vim.cmd[[highlight VertSplit guibg=NONE]]
