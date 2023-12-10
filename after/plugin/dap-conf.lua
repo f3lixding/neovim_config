a83f0f707fd049c0f3f69a7f11673c0b62f7a355
Felix Ding <felixding1989@gmail.com>
2023-12-09 22:49:29 -0800

    Adds dap process launch adapter configuration

--- after/plugin/dap-conf.lua
+++ after/plugin/dap-conf.lua
@@ -8,34 +8,46 @@ vim.keymap.set('n', '<leader>dc', require('dap').disconnect)
 
 -- Configs for attaching to process
 local dap = require('dap')
-local buffer_types = { 'rust' }
+local buffer_types = { 'rust', 'c', 'cpp' }
 
 for _, buf_type in ipairs(buffer_types) do
   dap.configurations[buf_type] = {
       {
-        -- If you get an "Operation not permitted" error using this, try disabling YAMA:
-        --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-        name = "Attach to process",
-        type = buf_type,  -- Adjust this to match your adapter name (`dap.adapters.<name>`)
-        request = 'attach',
-        pid = require('dap.utils').pick_process,
-        args = {},
+          -- If you get an "Operation not permitted" error using this, try disabling YAMA:
+          --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+          name = "Attach to process",
+          type = buf_type, -- Adjust this to match your adapter name (`dap.adapters.<name>`)
+          request = 'attach',
+          pid = require('dap.utils').pick_process,
+          args = {},
+      },
+      {
+          name = "Launch a process",
+          type = buf_type, -- Adjust this to match your adapter name (`dap.adapters.<name>`)
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          args = {},
       },
   }
 
-  dap.adapters[buf_type] = require('fd.plugin-conf.rust-tools-conf').dap
+  if buf_type == 'rust' or buf_type == 'c' or buf_type == 'cpp' then
+    -- for now, because the only things I write are handled by lldb-vscode, so this will do.
+    dap.adapters[buf_type] = require('fd.plugin-conf.rust-tools-conf').dap
+  end
 end
 
--- dap ui 
+-- dap ui
 vim.keymap.set('n', '<leader>du', require('dapui').toggle)
 require('dapui').setup({
-  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-  mappings = {
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t"
-  },
+    icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+    mappings = {
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t"
+    },
 })
