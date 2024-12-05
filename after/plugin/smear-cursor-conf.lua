382d52bc290073c48d11392ae1a832263ef10171
Felix Ding <felixding1989@gmail.com>
2024-12-05 17:49:34 -0500

    Adds cursor smear

---
+++ after/plugin/smear-cursor-conf.lua
@@ -0,0 +1,8 @@
+require('smear_cursor').setup({
+  cursor_color = "#ff8800",
+  stiffness = 0.3,
+  trailing_stiffness = 0.1,
+  trailing_exponent = 3,
+  gamma = 1,
+  volume_reduction_exponent = -0.1,
+})
