2893ef135eb95107333d9b8355daf6d87136bcf6
Felix Ding <felixding1989@gmail.com>
2025-03-23 11:23:18 -0700

    changes to auto completion to use codestral

--- after/plugin/avante-conf.lua
+++ after/plugin/avante-conf.lua
@@ -1,6 +1,15 @@
 require('avante').setup({
-
-  auto_suggestions_provider = "claude",
+  auto_suggestions_provider = "codestral",
+  vendors = {
+    ["codestral"] = {
+      __inherited_from = 'openai',              -- Inherit from OpenAI-compatible provider
+      endpoint = 'https://api.mistral.ai/v1',   -- Mistral API endpoint
+      api_key_name = 'CODESTRAL_API_KEY',       -- Your Codestral API key
+      model = 'codestral-latest',                      -- Codestral model
+      temperature = 0,
+      max_tokens = 4096,
+    }
+  },
   claude = {
     endpoint = "https://api.anthropic.com",
     model = "claude-3-7-sonnet-20250219",
