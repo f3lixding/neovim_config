df4e6885faea2e95e4a4ab79aed0855904e3a036
Felix Ding <felixding1989@gmail.com>
2025-06-28 22:20:46 -0700

    silences window resize

--- lua/fd/mappings.lua
+++ lua/fd/mappings.lua
@@ -1,31 +1,30 @@
 -- This makes yank copy from cursor pos to EOL
-vim.api.nvim_set_keymap('n', 'Y', 'y$', {noremap = true})
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
 
 -- Better window resize
-vim.api.nvim_set_keymap('n', '∆', ':resize -2<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '˚', ':resize +2<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '˙', ':vertical resize +2<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '¬', ':vertical resize -2<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '∆', ':resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '˚', ':resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '˙', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '¬', ':vertical resize -2<CR>', { noremap = true, silent = true })
 
 -- For when you use the laptop's keyboard
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
-vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {noremap = true})
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
 
 -- Better tabbing
-vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true})
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
 
 -- Better window navigation
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
 
 -- Better terminal instantiation
-vim.api.nvim_set_keymap('n', '<leader>tj', '<cmd>bo split<CR><cmd>term<CR><cmd>res 20<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>tj', '<cmd>bo split<CR><cmd>term<CR><cmd>res 20<CR>', { noremap = true })
 
 -- Nvim tree commands
-vim.api.nvim_set_keymap('n', '<leader>fe', ':NvimTreeToggle<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true})
-
+vim.api.nvim_set_keymap('n', '<leader>fe', ':NvimTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true })
