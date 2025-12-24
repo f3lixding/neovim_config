6909ddc03c8f4540c8df4cd6178d53540b981cb4
Felix Ding <felixding1989@gmail.com>
2025-12-24 12:36:39 -0800

    updates config for treesitter

--- after/plugin/avante-conf.lua
+++ after/plugin/avante-conf.lua
@@ -1,27 +1,33 @@
 require('avante').setup({
   auto_suggestions_provider = "claude-haiku",
-  vendors = {
+  providers = {
     ["codestral"] = {
       __inherited_from = 'openai',            -- Inherit from OpenAI-compatible provider
       endpoint = 'https://api.mistral.ai/v1', -- Mistral API endpoint
       api_key_name = 'CODESTRAL_API_KEY',     -- Your Codestral API key
       model = 'codestral-latest',             -- Codestral model
-      temperature = 0,
-      max_tokens = 8192,
+      extra_request_body = {
+        max_tokens = 8192,
+        temperature = 0,
+      }
     },
     ["claude-haiku"] = {
       __inherited_from = "claude",
       model = "claude-3-5-haiku-20241022",
       timeout = 30000, -- Timeout in milliseconds
-      temperature = 0,
-      max_tokens = 16384,
+      extra_request_body = {
+        max_tokens = 16384,
+        temperature = 0,
+      }
     },
-  },
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-sonnet-4-20250514",
-    temperature = 0,
-    max_tokens = 8192,
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-sonnet-4-20250514",
+      extra_request_body = {
+        max_tokens = 8192,
+        temperature = 0,
+      }
+    },
   },
   mappings = {
     suggestion = {
--- after/plugin/nvim-treesitter-conf.lua
+++ after/plugin/nvim-treesitter-conf.lua
@@ -1,11 +1,9 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "rust", "zig" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = {},
-  highlight = {
-    enable = true,
-    disable = {},
-    additional_vim_regex_highlighting = true
-  },
-}
+-- Since the update, treesitter no longer requires explicit set up call
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+-- This call is a noop once these parsers are installed once
+require 'nvim-treesitter'.install { 'rust', 'javascript', 'zig', 'lua', 'typescript' }
