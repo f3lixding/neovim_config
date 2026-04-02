d063cf72dce8a75f549ec2c400fb9bf2ed411944
Felix Ding <felixding1989@gmail.com>
2026-04-01 23:18:14 -0700

    removes ai assistant related plugins and start up plugin

--- lua/fd/plugins.lua
+++ lua/fd/plugins.lua
@@ -8,21 +8,20 @@ Plug 'morhetz/gruvbox'
 Plug 'nvim-tree/nvim-tree.lua'
 Plug 'nvim-tree/nvim-web-devicons'
 Plug 'airblade/vim-gitgutter'
-Plug 'max397574/startup.nvim'
 
 -- lsp and autocomplete
 Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/nvim-cmp'   -- autocomplete main package
+Plug 'hrsh7th/nvim-cmp' -- autocomplete main package
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-nvim-lua'
-Plug 'hrsh7th/cmp-vsnip'                     -- snipping engine
-Plug 'hrsh7th/vim-vsnip'                     -- also for snipping engine
-Plug 'hrsh7th/cmp-nvim-lsp-signature-help'   -- for function signature info
+Plug 'hrsh7th/cmp-vsnip'                   -- snipping engine
+Plug 'hrsh7th/vim-vsnip'                   -- also for snipping engine
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help' -- for function signature info
 Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'rmagatti/goto-preview'                 -- pop up go to definition
-Plug 'stevearc/aerial.nvim'                  -- file symbol
+Plug 'rmagatti/goto-preview'               -- pop up go to definition
+Plug 'stevearc/aerial.nvim'                -- file symbol
 
 -- rust related
 Plug 'mrcjkb/rustaceanvim'
@@ -35,18 +34,14 @@ Plug 'windwp/nvim-autopairs'
 
 -- telescope
 Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-lua/plenary.nvim'   -- required by telescope
+Plug 'nvim-lua/plenary.nvim' -- required by telescope
 Plug 'ThePrimeagen/harpoon'
 Plug 'nvim-telescope/telescope-live-grep-args.nvim'
 Plug('nvim-telescope/telescope-fzf-native.nvim',
-  { ['do'] =
-  'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' })
-
--- AI assistant
-Plug 'stevearc/dressing.nvim'
-Plug 'MeanderingProgrammer/render-markdown.nvim'
-Plug 'HakonHarnes/img-clip.nvim'
-Plug 'zbirenbaum/copilot.lua'
+  {
+    ['do'] =
+    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  })
 
 -- Debugging
 Plug 'mfussenegger/nvim-dap'
@@ -55,7 +50,7 @@ Plug 'nvim-neotest/nvim-nio'
 
 -- terminal utils
 Plug 'folke/noice.nvim'
-Plug 'MunifTanjim/nui.nvim'   -- this is required by noice
+Plug 'MunifTanjim/nui.nvim' -- this is required by noice
 Plug 'rcarriga/nvim-notify'
 Plug 'hrsh7th/cmp-cmdline'
 
